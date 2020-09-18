3207f9ccaecbce4fbdaf4525e12cf489ed693fbc
Yaroslav Andriyenko <andriyenko.yaroslav@gmail.com>
2020-09-18 18:38:26 +0300

    Home component was added

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Home } from './components/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Dedicatted test task</Text>
+      <Home />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +15,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ components/Home.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import {
+  StyleSheet, View, Text, ScrollView,
+  Image, Platform, StatusBar
+} from 'react-native';
+import Animated from 'react-native-reanimated';
+
+const images = [
+  {
+    id: 1,
+    uri: require('../assets/image_1.jpg')
+  },
+  {
+    id: 2,
+    uri: require('../assets/image_2.jpg')
+  },
+  {
+    id: 3,
+    uri: require('../assets/image_3.jpg')
+  }
+];
+
+const HEADER_HEIGHT = Platform.OS === 'ios' ? 115 : 70;
+
+export function Home() {
+  const scrollY = new Animated.Value(0);
+  const diffClampScrollY = Animated.diffClamp(scrollY, 0, HEADER_HEIGHT);
+  const headerY = Animated.interpolate(diffClampScrollY, {
+    inputRange: [0, HEADER_HEIGHT],
+    outputRange: [0, -HEADER_HEIGHT]
+  });
+
+  return (
+    <View style={styles.wrapper}>
+      <Animated.View
+        style={{
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          top: 0,
+          height: HEADER_HEIGHT,
+          backgroundColor: 'grey',
+          zIndex: 1000,
+          elevation: 1000,
+          transform: [{ translateY: headerY }],
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Text>
+          Header
+        </Text>
+      </Animated.View>
+      <Animated.ScrollView
+        bounces={false}
+        scrollEventThrottle={16}
+        style={styles.scrollContainer}
+        onScroll={
+          Animated.event([
+            {
+              nativeEvent:{
+                contentOffset: { y: scrollY }
+              }
+            }
+          ])
+        }
+      >
+        {images.map(image => (
+          <View
+            key={image.id}
+            style={styles.imageContainer}
+          >
+            <Image
+              source={image.uri}
+              style={styles.image}
+            />
+          </View>
+        ))}
+      </Animated.ScrollView>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1
+  },
+  imageContainer: {
+    height: 400,
+    margin: 20,
+  },
+  image: {
+    flex: 1,
+    height: null,
+    width: null,
+    borderRadius: 10,
+  },
+  scrollContainer: {
+    paddingTop: HEADER_HEIGHT
+  }
+});
